8d4e95665e31e73bb4ca83e9aa3c0c6510914f9b
Lima 🍋‍🟩 <polychimpz@hotmail.com>
2025-07-19 23:31:51 +0700

    add vault page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,6 +14,7 @@
     { name: 'About', href: '/about' },
     { name: 'Gallery', href: '/gallery' },
     { name: 'Stake', href: '/stake' },
+    { name: 'Vault', href: '/vault' },
     { name: 'Shop', href: '/shop' },
     { name: 'DAO', href: '/dao' },
   ];
